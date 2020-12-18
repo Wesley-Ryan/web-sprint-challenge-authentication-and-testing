c3ae7b2a91b7328b3772e14bf89db91e3486ac16
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-17 22:30:32 -0800

    Tests for auth endpoints are passing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,49 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const request = require("supertest");
+const server = require("./server");
+const db = require("../data/dbConfig");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db("users").truncate();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
+const invalidUser = { username: "RickJames" };
+const validUser = { username: "RickJames", password: "password" };
+
+describe("Authentication Endpoints", () => {
+  describe("[POST] REGISTER", () => {
+    it("/register responds with 200 if username and password are valid", async () => {
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(validUser);
+      expect(response.status).toBe(201);
+    });
+    it("/register responds with '' if missing fields", async () => {
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(invalidUser);
+      expect(JSON.stringify(response.body)).toMatch(
+        /username and password required/
+      );
+    });
+  });
+  describe("[POST] LOGIN", () => {
+    it("/login responds with token if user is registerd", async () => {
+      const register = await request(server)
+        .post("/api/auth/register")
+        .send(validUser);
+      const res = await request(server).post("/api/auth/login").send(validUser);
+      expect(res.body.token).toHaveLength(175);
+    });
+    it("/login responds with 'invalid credentials' if not registered", async () => {
+      const res = await request(server).post("/api/auth/login").send(validUser);
+      expect(JSON.stringify(res.body)).toMatch(/invalid credentials/);
+    });
+  });
+});
